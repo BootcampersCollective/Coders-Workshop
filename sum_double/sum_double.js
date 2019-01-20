d615b0a443221a68259c5d054db4b0b108cfdcc5
Andy Young <ayscript@gmail.com>
2019-01-20 09:19:35 -0700

    complete sum_double

---
+++ sum_double/sum_double.js
@@ -0,0 +1,8 @@
+// Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+function sum_double(n1, n2) {
+	return n1 === n2 ? (n1 + n2) * 2 : n1 + n2;
+}
+
+sum_double(2, 4);  // 6
+sum_double(5, 5);  // 20
