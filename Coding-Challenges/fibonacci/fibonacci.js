1fe8447ef1bd6e0cff06a6f7da5a7463258f23e6
Andy Young <ayscript@gmail.com>
2019-01-30 11:42:30 -0700

    Complete 'start' branch sans solutions

--- Coding-Challenges/fibonacci/fibonacci.js
+++ Coding-Challenges/fibonacci/fibonacci.js
@@ -1,36 +1,5 @@
 function productFib(prod) {
-  let factors = [],
-    i = 0,
-    k = 1,
-    q = k;
-
-  function fib() {
-    while (k <= prod) {
-      q = k;
-      k += i;
-      i = q;
-      factors.push(k);
-      return fib();
-    }
-  }
-  fib();
-
-  let result = factors.filter(
-    (el, i, arr) => el * arr[i + 1] === prod || el * arr[i - 1] === prod
-  );
-
-  if (result.length === 0) {
-    result = factors.filter(
-      (el, i, arr) => el * arr[i + 1] > prod || el * arr[i - 1] > prod
-    );
-
-    result = result.splice(0, 2);
-    result.push(false);
-    return result;
-  } else {
-    result.push(true);
-    return result;
-  }
+  
 }
 
 productFib(5895);
--- Coding-Challenges/sumDouble/sumDouble.js
+++ Coding-Challenges/sumDouble/sumDouble.js
@@ -1,7 +1,7 @@
 // Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
 function sum_double(n1, n2) {
-	return n1 === n2 ? (n1 + n2) * 2 : n1 + n2;
+	
 }
 
 sum_double(2, 4);  // 6
