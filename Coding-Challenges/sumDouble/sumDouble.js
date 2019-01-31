c9823ba11ea1880715f2975d9ec6aa4fba8f9a6e
Badr Choubai <badr.choubaidev@outlook.com>
2019-01-31 08:42:01 -0700

    sum_double

--- Coding-Challenges/sumDouble/sumDouble.js
+++ Coding-Challenges/sumDouble/sumDouble.js
@@ -1,8 +1,11 @@
 // Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
-function sum_double(n1, n2) {
-	
-}
+const sum_double = (n1, n2) => n1 == n2 ? (n1 + n2) * 2 : n1 + n2;
 
-sum_double(2, 4);  // 6
-sum_double(5, 5);  // 20
+console.log(
+  sum_double(2, 4)  // 6
+)
+
+console.log(
+  sum_double(5, 5)  // 20
+)
