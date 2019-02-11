aed1252d364808bb429043747f38114374a85912
Adam Vinueza <adam.vinueza@mindflash.com>
2019-02-11 16:11:29 -0700

    First pass through revamping tests

--- collaborators/AdamVinueza/cryptogram/run_test.py
+++ collaborators/AdamVinueza/cryptogram/run_test.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 import cryptogram
 
+test1 = {
+    'word_set': [ 'apple', 'able', 'ale', 'bale', 'aple', 'kangaroo' ],
+    'target': 'abppplee',
+    'longest_word': 'apple'
+}
+
+def run_test(test):
+    pass
+
+test2 = {
+    'word_set': [],
+    'target': 'apple',
+    'longest_word': None
+}
+
 if __name__ == '__main__':
-    word_set = [ 'apple', 'able', 'ale', 'bale', 'aple', 'kangaroo' ]
-    target_word = 'abppplee'
     longest_word = cryptogram.get_longest_subsequence(target_word, word_set)
     print(longest_word if longest_word else 'No longest subsequence')
