7729b6953d255fd0697f88e9e7e65106c5a7fdd8
Adam Vinueza <adam.vinueza@mindflash.com>
2019-02-13 10:08:00 -0700

    Added some command-line choices for algorithm to use

--- collaborators/AdamVinueza/coinFlip/coin_flip.py
+++ collaborators/AdamVinueza/coinFlip/coin_flip.py
@@ -13,11 +13,11 @@ always take the ceiling.
 (The ceiling of a number n = the smallest integer >= n.)
 '''
 
-def expected_flips_tail(n, accum):
+def _expected_flips_tail(n, accum):
     '''Counts expected flips using tail recursion.'''
-    return expected_flips_tail(math.ceil(n/2), accum + 1) if n > 1 else accum
+    return _expected_flips_tail(math.ceil(n/2), accum + 1) if n > 1 else accum
 
-def expected_flips_with_bit_shift(n, accum):
+def _expected_flips_with_bit_shift(n, accum):
     '''
     We can use bit shifting here instead of division if we add 1 to the
     shifted number if the number being shifted is odd. This is equivalent to
@@ -26,20 +26,32 @@ def expected_flips_with_bit_shift(n, accum):
     if n > 1:
         shifted = n >> 1
         next_number = 1 + shifted if n & 1 else shifted
-        return expected_flips_with_bit_shift(next_number, accum + 1)
+        return _expected_flips_with_bit_shift(next_number, accum + 1)
     else:
         return accum
 
 def expected_flips(n):
     '''Counts expected flips.'''
-    return expected_flips_tail(n, 0)
+    return 1 + expected_flips(math.ceil(n/2)) if n > 1 else 0
+
+def expected_flips_bit_shifted(n):
+    return _expected_flips_with_bit_shift(n, 0)
+
+def expected_flips_optimized(n):
+    return _expected_flips_tail(n, 0)
 
 if __name__ == '__main__':
+    coin_flippers = {
+        'tail_optimized': expected_flips_optimized,
+        'bit_shifted': expected_flips_bit_shifted,
+        'standard': expected_flips
+    }
     parser = argparse.ArgumentParser()
     parser.add_argument('number_of_coins', help='number of coins at start')
+    parser.add_argument('-a', '--algorithm', help='expectation algorithm',
+        default='tail_optimized')
     args = parser.parse_args()
-    if not args.number_of_coins:
-        parser.error()
     number_of_coins = int(args.number_of_coins)
+    func = coin_flippers[args.algorithm] if args.algorithm else expected_flips
     print('Expected number of flips until one coin is left: {0}'.format(
-        expected_flips(number_of_coins)))
+        func(number_of_coins)))
