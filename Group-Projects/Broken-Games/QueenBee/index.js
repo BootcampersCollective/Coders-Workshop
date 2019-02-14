e8fa3d327542cc57c58b0dadeeb6012aa23b81f2
Andy Young <ayscript@gmail.com>
2019-02-14 15:18:58 -0700

    Get circle function working

--- Group-Projects/Broken-Games/QueenBee/index.js
+++ Group-Projects/Broken-Games/QueenBee/index.js
@@ -1,14 +1,23 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let position = 0;
+// let position = 0; // swap w/ 'size' to
+// let size = 0;     // grow sprite to fill canvas
 
-setInterval(function() {
-  ctx./*put 'context' here*/clearRect(0, 0, 500, 500);
-  ctx.fillRect(position, 0, 20, 20);
+// setInterval(function() {
+//   ctx./*put 'context' here*/clearRect(0, 0, 500, 500);
+//   ctx.fillRect(0, 0, size, size); // swap size for sprite dimensions
 
-  position++;
-  if (position > 500) {
-    position = 0;
-  }
-}, 30);
+//   size++;
+//   if (size > 500) {
+//     size = 0;
+//   }
+// }, 30);
+
+const circle = (x, y, radius, fillCircle) => {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+
+  fillCircle ? ctx.fill() : ctx.stroke();
+};
+circle(250, 250, 50, true);
