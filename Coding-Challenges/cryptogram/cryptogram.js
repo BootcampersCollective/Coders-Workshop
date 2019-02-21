4dbdee02734a54b575dd8abbb12e88e7e329b52d
Bryan Yunis <bbyunis@gmail.com>
2019-02-20 19:53:55 -0700

    add cryptogram solution bryan-yunis

--- Coding-Challenges/cryptogram/cryptogram.js
+++ Coding-Challenges/cryptogram/cryptogram.js
@@ -1,5 +1,34 @@
-function cryptogram(str, arr) {
-}
+const cryptogram = (s, d) => {
+  let resultsarr = [];
+  let max = 0;
+  let longestword = "";
+
+  d.forEach(word => {
+    let spointer = 0;
+    let wordpointer = 0;
+
+    while (wordpointer < word.length) {
+      if (word[wordpointer] === s[spointer]) {
+        wordpointer++;
+        spointer++;
+      } else {
+        spointer++;
+      }
+      // letter is not contained in s, move to next word
+      if (spointer === s.length) {
+        break;
+      }
+      // each letter of word is contained in s
+      else if (wordpointer === word.length - 1) {
+        if (word.length > max) {
+          max = word.length;
+          longestword = word;
+        }
+      }
+    }
+  });
+  return longestword;
+};
 
 console.log(
   cryptogram("abpplee", ["able", "ale", "apple", "bale", "kangaroo"])
