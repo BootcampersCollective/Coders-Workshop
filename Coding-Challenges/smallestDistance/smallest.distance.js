a30ef1e3b2a7326318e038aea5ea9938a0f55f0d
bry-an <bbyunis@gmail.com>
2019-02-28 21:51:44 -0700

    add smallestDistance question

---
+++ Coding-Challenges/smallestDistance/smallest.distance.js
@@ -0,0 +1,5 @@
+function smallestDistance(words, string) {
+
+}
+
+console.log(smallestDistance("hello world", "dog cat hello cat dog dog hello cat world")) //1 
---
+++ Coding-Challenges/smallestDistance/smallestDistance.py
@@ -0,0 +1,5 @@
+def smallest_distance:
+
+
+
+print(smallest_distance("hello world", "dog cat hello cat dog dog hello cat world")) # 1
