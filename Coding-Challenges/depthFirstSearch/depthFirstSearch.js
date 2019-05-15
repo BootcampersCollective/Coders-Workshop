97c1e7f2382d45b543dac9c80e80297a088d11f1
Bryan Yunis <bbyunis@gmail.com>
2019-05-15 15:07:31 -0600

    Update depthFirstSearch.js

--- Coding-Challenges/depthFirstSearch/depthFirstSearch.js
+++ Coding-Challenges/depthFirstSearch/depthFirstSearch.js
@@ -3,8 +3,8 @@
 You are given a Node class that has a name and an array of optional children Nodes. When put together, Nodes form a simple tree-like structure. Implement the depthFirstSearch method on the Node class, which takes in an empty array, traverses the tree using the Depth-first Search approach (specifically navigating the tree from left to right), stores all the of the Nodes' names in the input array, and returns it.
 
 input:
-		A
-	  /	| \
+	A
+      /	| \
      B	C  D
     / \	  / \
    E  F  G   H
