311d29e387c6755058c159236beddb82bf7d9d56
Kellan Loew <kellanloew@gmail.com>
2019-06-19 21:51:31 -0600

    Additional functionality

--- collaborators/KellanLoew/testing.js
+++ collaborators/KellanLoew/testing.js
@@ -1,8 +1,20 @@
-var assert = require('assert'); //Access Node's 
+var assert = require('assert'); //Access Node's test library
 
 function Add(string){
 
     var numbers = string.split(",");
+    var length = numbers.length;
+    
+    for(var i = 0; i < length; i++){
+        if(numbers[i].includes("\n")){
+            newLine = numbers[i].split("\n");
+            for(var j = 1; j < newLine.length; j++){
+                numbers.push(newLine[j]);
+                length ++;
+            }
+        }
+    }
+
     var sum = 0; 
     
     for(var i = 0; i < numbers.length; i++){
@@ -17,4 +29,6 @@ assert.equal(Add(""), 0);
 assert.equal(Add("34"), 34);
 assert.equal(Add("3,"), 3);
 assert.equal(Add("4,2"), 6);
-assert.equal(Add("4,2,8,5"), 19);
+assert.equal(Add("1\n4,2"), 7);
+assert.equal(Add("4,2\n8\n5"), 19);
+assert.equal(Add("4,2\n8\n5,1"), 20);
