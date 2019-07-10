f0ae3599cff57abe539865c1c4913b122688e17b
Bryan Yunis <bryany@booj.com>
2019-07-10 12:40:37 -0600

    fix syntax error in prefixmapsum

--- collaborators/BryanYunis/solutions/prefixMapSum.py
+++ collaborators/BryanYunis/solutions/prefixMapSum.py
@@ -2,8 +2,14 @@ class PrefixMapSum:
     def __init__(self):
         self.map = {}
 
-    def insert(self, key: str, value: int):
+    def insert(self, key, value):
         self.map[key] = value
 
     def sum(self, prefix):
         return sum(value for key, value in self.map.items() if key.startswith(prefix))
+
+mapsum = PrefixMapSum()
+mapsum.insert("columnar", 3)
+assert mapsum.sum("col") == 3
+mapsum.insert("column", 2)
+assert mapsum.sum("col") == 5
