bb58f7eab3c94acdfb9b9b2376952e6fa15d5381
Bryan Yunis <bbyunis@gmail.com>
2019-07-31 08:09:01 -0600

    add hash table challenges

---
+++ Coding-Challenges/allUniqueCharacters/allUniqueCharacters.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+const allUnique = str => {
+
+}
+
+assert.equal(allUnique('whistle'), true);
+assert.equal(allUnique('painting'), false);
+
+
---
+++ Coding-Challenges/permutationOfAPalindrome/permutationOfAPalindrome.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+const permutationOfAPalindrome = str => {
+
+}
+
+assert.equal(permutationOfAPalindrome('racecar'), true);
+assert.equal(permutationOfAPalindrome('tactcoa'), true);
+assert.equal(permutationOfAPalindrome('kitten'), false);
