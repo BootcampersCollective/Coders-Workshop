79d9c8b83b856e77e55182c884d4954cbdbab88c
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-09-06 18:10:22 -0600

    Solve Warehouse Optimization in JS

---
+++ collaborators/MartinRosenberg/warehouse-optimization/README.md
@@ -0,0 +1,4 @@
+# Warehouse Optimization
+
+*By request, this prompt is private. The solution is provided for the benefit of other people with access to the prompt.*
+
---
+++ collaborators/MartinRosenberg/warehouse-optimization/warehouse-optimization.js
@@ -0,0 +1,25 @@
+import { OrderRepository } from './order-repository'
+
+const main = () => {
+  const orderRepo = new OrderRepository()
+  const orders = orderRepo.getYesterdaysOrders()
+  
+  const combinedOrders = {}
+  
+  orders.forEach(({ orderLines }) => {
+    orderLines.forEach(({ itemName, quantity }) => {
+      if (combinedOrders[itemName]) {
+        combinedOrders[itemName] += quantity
+      } else {
+        combinedOrders[itemName] = quantity
+      }
+    })
+  })
+  
+  Object.entries(combinedOrders)
+    .sort((a, b) => b[1] - a[1])
+    .forEach(([k, v], i) => {
+      console.log(`Shelf ${i + 1}:\t${k}`)
+    })
+}
+
