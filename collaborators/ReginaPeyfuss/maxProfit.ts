9ab6886472dcf529ca8c9ed41ad92001d8e528cc
Regina Peyfuss <rpeyfuss@gmail.com>
2019-10-05 21:49:08 -0600

    added problem description

--- collaborators/ReginaPeyfuss/maxProfit.ts
+++ collaborators/ReginaPeyfuss/maxProfit.ts
@@ -1,8 +1,14 @@
 
 /**
-* This will calculate the max profit of an array in linear time
-*
-/*
+* Given an array of numbers representing the stock prices of a company in chronological order,
+* write a function that calculates the maximum profit you could have made from buying and selling
+* that stock once. You must buy before you can sell it.
+* e.g. given the array [9, 11, 8, 5, 7, 10], you should return 5, since that's the greatest profit that
+* can be made.
+* Try doing this problem without using a nested for-loop, i.e. only iterating through the array once.
+* (This is referred to as being done in O(n) time, whereas a nested for-loop would be O(n^2).)
+*/
+
 export class MaxProfit {
   static maxProfitForward(list: number[]) {
     if (!list || list.length < 1) return 0;
