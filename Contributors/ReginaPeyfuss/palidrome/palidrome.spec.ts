9721d22c699620d5cfc129aba1ed1d01e7ca7b84
Regina Peyfuss <rpeyfuss@gmail.com>
2020-04-01 19:55:25 -0600

    isPalidrome for number solution - fixed infinite loop

--- Contributors/ReginaPeyfuss/palidrome/palidrome.spec.ts
+++ Contributors/ReginaPeyfuss/palidrome/palidrome.spec.ts
@@ -1,11 +1,14 @@
 
 describe('Palidrome', () => {
-    it('should check if number is a palidrome', () => {
+    it('should check if number is a paladrome', () => {
         let isPaladrome = Palidrome.isPalindrome(121);
         expect (isPaladrome).toEqual(true);
 
         isPaladrome = Palidrome.isPalindrome(123);
         expect (isPaladrome).toEqual(false);
+
+        isPaladrome = Palidrome.isPalindrome(12321);
+        expect (isPaladrome).toEqual(true);
 
         isPaladrome = Palidrome.isPalindrome(undefined);
         expect (isPaladrome).toEqual(false);
--- Contributors/ReginaPeyfuss/palidrome/palidrome.ts
+++ Contributors/ReginaPeyfuss/palidrome/palidrome.ts
@@ -2,11 +2,14 @@
 export class Palidrome {
 
     static isPalindrome(num: number): boolean {
+        if (num === undefined || num === null || num < 10) return false;
         let x = num;
-        while (x !== 0){
+        while (x > 9){
             let lastDigit = x % 10;
-            let totalNumDigits = Math.log10(x);
+            let totalNumDigits = Math.trunc(Math.log10(x));
             let firstDigit = Math.trunc(x / Math.pow(10, totalNumDigits));
+            x = Math.trunc(x/10);
+            x = Math.trunc(x/(Math.pow(10, totalNumDigits-1)));
             if (firstDigit !== lastDigit) return false;
         }
         return true;
