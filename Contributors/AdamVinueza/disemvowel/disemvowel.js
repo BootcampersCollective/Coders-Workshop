1e9f36e2e163d698d39bdf26517be7710d30c2cc
adamvinueza <adamvinueza@pm.me>
2020-05-06 18:35:05 -0600

    documentation

--- Contributors/AdamVinueza/disemvowel/disemvowel.js
+++ Contributors/AdamVinueza/disemvowel/disemvowel.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
+// A set of vowels (case-sensitive). We use a set because Set.has is much faster
+// than Array.indexOf.
 const vowels = new Set([ 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+
+// Remove all occurrences of any member of set from src.
 const removeOccurrences = (src, set) => {
   if (!src) {
     return src;
@@ -14,13 +18,16 @@ const removeOccurrences = (src, set) => {
   return dest.join('');
 };
 
+// Disemvowel removes all occurrences of vowels from src.
 const disemvowel = src => {
   return removeOccurrences(src, vowels);
 }
 
+// Test strings, aligned with the expected values.
 const test = [ 'quick', 'banana', undefined, '', 'aeiouaeiouAEIOU' ];
 const expecteds = [ 'qck', 'bnn', undefined, '', '' ]
 
+// Run the tests.
 let passed = true;
 for (let i = 0; i < test.length; i++) {
   actual = disemvowel(test[i]);
