eb41744e40b807d38d01621545c85968aa4be9f4
Bryan Yunis <bryany@booj.com>
2021-04-24 11:58:41 -0600

    add largestRectangleInMatrix challenge and sol'n

---
+++ Coding-Challenges/largestRectangleInMatrix/README.md
@@ -0,0 +1,16 @@
+# Largest Rectangle In Matrix
+
+Given an N by M matrix consisting only of 1's and 0's, find the largest rectangle containing only 1's and return its area.
+
+For example, given the following matrix:
+
+```js
+;[
+  [1, 0, 0, 0],
+  [1, 0, 1, 1],
+  [1, 0, 1, 1],
+  [0, 1, 0, 0]
+]
+```
+
+Return 4.
---
+++ Contributors/BryanYunis/solutions/largestRectangleInMatrix/largestRectangleInMatrix.js
@@ -0,0 +1,73 @@
+const assert = require('assert')
+
+const getLength = (vertical) => (row, col, matrix) => {
+  let count = 0
+  while (matrix[row] && matrix[row][col] && matrix[row][col] === 1) {
+    count += 1
+    if (vertical) {
+      row += 1
+    } else {
+      col += 1
+    }
+  }
+  return count
+}
+
+const getHorizontalLength = getLength(false)
+const getVerticalLength = getLength(true)
+
+const findLargestRect = (matrix) => {
+  let maxRectangleSize = 0
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] === 1) {
+        const horizontalLength = getHorizontalLength(row, col, matrix)
+        let i = 0
+        let minVerticalLength = null
+        while (i < horizontalLength) {
+          const verticalLength = getVerticalLength(row, col + i, matrix)
+          if (!minVerticalLength) {
+            minVerticalLength = verticalLength
+          }
+          minVerticalLength = Math.min(minVerticalLength, verticalLength)
+          i++
+        }
+        maxRectangleSize = Math.max(
+          horizontalLength * minVerticalLength,
+          maxRectangleSize
+        )
+      }
+    }
+  }
+  return maxRectangleSize
+}
+
+assert.strictEqual(
+  findLargestRect([
+    [1, 0, 0, 0],
+    [1, 0, 1, 1],
+    [1, 0, 1, 1],
+    [0, 1, 0, 0]
+  ]),
+  4
+)
+assert.strictEqual(
+  findLargestRect([
+    [1, 0, 0, 0],
+    [1, 0, 1],
+    [1, 0, 1],
+    [0, 1, 0]
+  ]),
+  3
+)
+assert.strictEqual(
+  findLargestRect([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ]),
+  0
+)
+
+console.log('👍')
