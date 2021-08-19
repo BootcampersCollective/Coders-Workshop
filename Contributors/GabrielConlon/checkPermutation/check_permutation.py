b559668ecd4cf2e64dc38886eb915b9906f6c2bb
Gabe <3859926+gratefulgabe@users.noreply.github.com>
2021-08-19 13:59:53 -0600

    working unittest and main()
    completed with a comparison of char count within the strings.  would love feedback if this has a glaring hole I missed in logic.

--- Contributors/GabrielConlon/checkPermutation/check_permutation.py
+++ Contributors/GabrielConlon/checkPermutation/check_permutation.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
 def check_permutation(str1, str2):
-    pass
+    # include case-sensitive, whitespace, and UTF-8 encoding
+    # decode('UTF-8')
 
+    str1_charCount = {x: str1.count(x) for x in set(str1)}
+    str2_charCount = {x: str2.count(x) for x in set(str2)}
 
-print(check_permutation("god", "dog"))  # True
-print(check_permutation("hello", "goodbye"))  # False
+    # check str1==str2 for length and character count (i.e. abort if strings are 'abled' and 'aisle')
+    # if all character counts are equal, you have a matching string
+    if str1_charCount != str2_charCount:
+        return False
+    else:
+        return True
+
+def main():
+    print(check_permutation("god", "dog"))  # True
+    print(check_permutation("hello", "goodbye"))  # False
+    print(check_permutation("hello world", "goodbye world")) # False
+    print(check_permutation("hello world", "lll ooehrwd")) # False
+
+if __name__ == "__main__":
+    main()
